177010a3fc979071247cd65e20e0a354b1c63ba7
gustavoestevesr <gustavo.tech4me@gmail.com>
2023-02-02 12:28:16 -0300

    Inserido EmailModel e StatusEmailEnum

---
+++ src/main/java/com/ms/sendemailservice/enums/StatusEmail.java
@@ -0,0 +1,7 @@
+package com.ms.sendemailservice.enums;
+
+public enum StatusEmail {
+    PROCESSING,
+    SENT,
+    ERROR;
+}
---
+++ src/main/java/com/ms/sendemailservice/models/EmailModel.java
@@ -0,0 +1,31 @@
+package com.ms.sendemailservice.models;
+
+import javax.persistence.*;
+
+import com.ms.sendemailservice.enums.StatusEmail;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "TB_EMAIL")
+public class EmailModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private UUID emailId;
+    private String ownerRef;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    @Column(columnDefinition = "TEXT")
+    private String text;
+    private LocalDateTime sendDateEmail;
+    private StatusEmail statusEmail;
+}
+
