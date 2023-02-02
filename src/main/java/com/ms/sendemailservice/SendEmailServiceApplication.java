a7088d8a1c19cfc8157e7f2c9edebd876701561b
gustavoestevesr <gustavo.tech4me@gmail.com>
2023-02-02 12:23:15 -0300

    novo projeto spring boot

---
+++ src/main/java/com/ms/sendemailservice/SendEmailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ms.sendemailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendEmailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendEmailServiceApplication.class, args);
+	}
+
+}
