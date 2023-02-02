6f839f6dca4d79cd67e19d86e19587f69e4ad496
gustavoestevesr <gustavo.tech4me@gmail.com>
2023-02-02 12:36:04 -0300

    Inserido EmailController para envio de email atraves do metodo POST

---
+++ src/main/java/com/ms/sendemailservice/controllers/EmailController.java
@@ -0,0 +1,30 @@
+package com.ms.sendemailservice.controllers;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ms.sendemailservice.dtos.EmailDto;
+import com.ms.sendemailservice.models.EmailModel;
+import com.ms.sendemailservice.services.EmailService;
+
+@RestController
+public class EmailController {
+
+    @Autowired
+    EmailService emailService;
+
+    @PostMapping("/sending-email")
+    public ResponseEntity<EmailModel> sendingEmail(@RequestBody @Valid EmailDto emailDto) {
+        EmailModel emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailDto, emailModel);
+        emailService.sendEmail(emailModel);
+        return new ResponseEntity<>(emailModel, HttpStatus.CREATED);
+    }   
+}
