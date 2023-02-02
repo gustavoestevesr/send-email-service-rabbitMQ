019d47311d7ce3d577480e16d03b68d3d0a5a8ef
gustavoestevesr <gustavo.tech4me@gmail.com>
2023-02-02 15:46:19 -0300

    Insercao de novos metodos Find All e FindById em EmailController

--- src/main/java/com/ms/sendemailservice/controllers/EmailController.java
+++ src/main/java/com/ms/sendemailservice/controllers/EmailController.java
@@ -1,24 +1,31 @@
 package com.ms.sendemailservice.controllers;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ms.sendemailservice.dtos.EmailDto;
 import com.ms.sendemailservice.models.EmailModel;
-import com.ms.sendemailservice.services.EmailService;
+import com.ms.sendemailservice.services.EmailServiceImpl;
 
 @RestController
 public class EmailController {
 
     @Autowired
-    EmailService emailService;
+    EmailServiceImpl emailService;
 
     @PostMapping("/sending-email")
     public ResponseEntity<EmailModel> sendingEmail(@RequestBody @Valid EmailDto emailDto) {
@@ -26,5 +33,21 @@ public class EmailController {
         BeanUtils.copyProperties(emailDto, emailModel);
         emailService.sendEmail(emailModel);
         return new ResponseEntity<>(emailModel, HttpStatus.CREATED);
-    }   
+    }
+
+    @GetMapping("/emails")
+    public ResponseEntity<Page<EmailModel>> getAllEmails(
+            @PageableDefault(page = 0, size = 5, sort = "emailId", direction = Sort.Direction.DESC) Pageable pageable) {
+        return new ResponseEntity<>(emailService.findAll(pageable), HttpStatus.OK);
+    }
+
+    @GetMapping("/emails/{emailId}")
+    public ResponseEntity<Object> getOneEmail(@PathVariable(value = "emailId") UUID emailId) {
+        Optional<EmailModel> emailModelOptional = emailService.findById(emailId);
+        if (!emailModelOptional.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Email not found.");
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body(emailModelOptional.get());
+        }
+    }
 }
