0108fd4754e1acc93854217ea3b25cd123a805db
gustavoestevesr <gustavo.tech4me@gmail.com>
2023-02-03 15:39:25 -0300

    Inserido mensageria com RabbitMQ

---
+++ src/main/java/com/ms/sendemailservice/consumers/EmailConsumer.java
@@ -0,0 +1,26 @@
+package com.ms.sendemailservice.consumers;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import com.ms.sendemailservice.dtos.EmailDto;
+import com.ms.sendemailservice.models.EmailModel;
+import com.ms.sendemailservice.services.EmailService;
+
+@Component
+public class EmailConsumer {
+
+    @Autowired
+    EmailService emailService;
+
+    @RabbitListener(queues = "${spring.rabbitmq.queue}")
+    public void listen(@Payload EmailDto emailDto) {
+        EmailModel emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailDto, emailModel);
+        emailService.sendEmail(emailModel);
+        System.out.println("Email Status: " + emailModel.getStatusEmail().toString());
+    }
+}
